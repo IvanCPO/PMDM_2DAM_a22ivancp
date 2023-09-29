017c2e18d9a4b7950ed4d6dd9dde0117eade5c85
Ivan Cabaleiro Poceiro <a22ivancp@iessanclemente.net>
2023-09-29 12:30:44 +0200

    Second version

--- UD02_3Chronos/app/src/main/java/com/example/ud02_3chronos/MainActivity.kt
+++ UD02_3Chronos/app/src/main/java/com/example/ud02_3chronos/MainActivity.kt
@@ -2,10 +2,67 @@ package com.example.ud02_3chronos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+    lateinit var chrono: Chronometer
+    var running = false
+    var offset : Long = 0
+    val RUNNING_KEY = "running"
+    val OFFSET_KEY = "offset"
+    val BASE_KEY = "base"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        chrono = findViewById<Chronometer>(R.id.chrono)
+
+        if (savedInstanceState != null){
+            offset= savedInstanceState.getLong(OFFSET_KEY)
+            running= savedInstanceState.getBoolean(RUNNING_KEY)
+            chrono.base = savedInstanceState.getLong(BASE_KEY)
+            if (running) {
+                chrono.base = savedInstanceState.getLong(BASE_KEY)
+                chrono.start()
+            }else{
+                chrono.base = SystemClock.elapsedRealtime() - offset
+            }
+        }
+
+        var btnStart = findViewById<Button>(R.id.btn_start)
+        btnStart.setOnClickListener {
+            if (!running){
+                chrono.base = SystemClock.elapsedRealtime()-offset
+                chrono.start()
+                running = true
+            }
+        }
+
+        var btnPause = findViewById<Button>(R.id.btn_pause)
+        btnPause.setOnClickListener {
+            if (running){
+                offset=SystemClock.elapsedRealtime()-chrono.base
+                chrono.stop()
+                running = false
+
+            }
+        }
+
+        var btnRestart = findViewById<Button>(R.id.btn_restart)
+        btnRestart.setOnClickListener {
+            offset = 0
+            chrono.base = SystemClock.elapsedRealtime()
+
+
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean(RUNNING_KEY, running)
+        outState.putLong(OFFSET_KEY, offset)
+        outState.putLong(BASE_KEY, chrono.base)
+        super.onSaveInstanceState(outState)
     }
 }
